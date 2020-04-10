7a9d0853722a39b1e5d14efbecb230e45131fadd
jullisshh <63472509+jullisshh@users.noreply.github.com>
2020-04-10 22:03:10 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const cartButton = document.querySelector('#cart-button');
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+cartButton.addEventListener('click', function (event) {
+   modal.classList.add('active');
+})
+
+close.addEventListener('click', function (event) {
+   modal.classList.remove('active');
+})
+
+new WOW().init();
